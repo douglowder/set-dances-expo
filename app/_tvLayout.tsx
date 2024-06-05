daf01cdc6fdc7bca10866cf1a70f77c2ba881294
Douglas Lowder <douglowder@mac.com>
2024-06-04 23:40:36 -0700

    Separate layouts for TV and phone

---
+++ app/_tvLayout.tsx
@@ -0,0 +1,74 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useColorScheme } from 'react-native';
+
+import { useScale } from '@/hooks/useScale';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function TVLayout() {
+  const colorScheme = useColorScheme();
+  const scale = useScale();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <GestureHandlerRootView>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="(tunes)"
+              options={{
+                presentation: 'modal',
+                headerShown: false,
+                contentStyle: {
+                  width: '90%',
+                  alignSelf: 'center',
+                  marginTop: 50 * scale,
+                  marginBottom: 50 * scale,
+                },
+              }}
+            />
+            <Stack.Screen
+              name="(info)"
+              options={{
+                presentation: 'modal',
+                headerShown: false,
+                contentStyle: {
+                  width: '90%',
+                  alignSelf: 'center',
+                  marginTop: 50 * scale,
+                  marginBottom: 50 * scale,
+                },
+              }}
+            />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </GestureHandlerRootView>
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
+}
