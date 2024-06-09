789a2d8811853f89ffa37bb4862b146faf91b785
Douglas Lowder <douglowder@mac.com>
2024-06-09 14:45:15 -0700

    Pivot credentials for TV and phone

---
+++ scripts/prepare-credentials.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+/**
+ * This script is used to reset the project to a blank state.
+ * It moves the /app directory to /app-example and creates a new /app directory with an index.tsx and _layout.tsx file.
+ * You can remove the `reset-project` script from package.json and safely delete this file after running it.
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+const root = process.cwd();
+
+const isTV = process.env.EXPO_TV === '1';
+
+const credentialsJsonSourcePath = path.join(
+  root,
+  isTV ? 'credentials_tv.json' : 'credentials_phone.json',
+);
+const credentialsJsonDestPath = path.join(root, 'credentials.json');
+
+fs.copyFileSync(credentialsJsonSourcePath, credentialsJsonDestPath);
+
+console.log(
+  `${path.basename(credentialsJsonSourcePath)} copied to ${path.basename(
+    credentialsJsonDestPath,
+  )}`,
+);
