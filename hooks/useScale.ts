fee3d741b4918eb30ca8edfc3617ca92be019c14
Douglas Lowder <douglowder@mac.com>
2024-07-04 23:32:48 -0700

    Add comments to useScale hook

--- hooks/useScale.ts
+++ hooks/useScale.ts
@@ -1,11 +1,18 @@
 import { useWindowDimensions } from 'react-native';
 
 export function useScale() {
+  // Screen height and width
   const { height, width } = useWindowDimensions();
+  // Scale all sizes, margins, padding, by this factor
   const scale = Math.max(width, height) / 1000;
+  // Screen aspect ratio (always > 1)
   const aspectRatio = Math.max(width, height) / Math.min(width, height);
+  // True for landscape views
   const landscape = width > height;
-  const tall = !landscape && aspectRatio > 1.7; // includes phone portrait but not ipad portrait
+  // This boolean selects for phones that are very tall compared to their width, and is
+  // used by the main screen to lay out some controls more vertically
+  const tall = !landscape && aspectRatio > 1.7;
+
   return {
     height,
     width,
